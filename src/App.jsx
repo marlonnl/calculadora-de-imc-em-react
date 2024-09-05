4f32b848028e588c8473edc0f7b4121eba5fbfed
Marlonn Locatelli <marlonn@gmail.com>
2024-09-05 19:14:29 -0300

    Commit inicial

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import Cabecalho from "./components/Cabecalho";
+import Calculadora from "./components/Calculadora";
+
+function App() {
+
+  return (
+    <>
+      <div className="container">
+        <Cabecalho />
+        <Calculadora />
+
+        <footer className="footer">
+          desenvolvido por <a href="https://github.com/marlonnl">marlonn</a>
+          </footer>
+      </div>
+    </>
+  )
+}
+
+export default App;
